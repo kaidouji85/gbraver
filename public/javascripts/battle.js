94e9292be5071f2225b0694281228c1d264a869b
竹内佑介 <yusuke@takeuchiyuusuke-no-MacBook-Air.local>
2014-02-02 16:46:35 +0900

    戦闘ロジッククラスのテストコードを追加した。

---
+++ public/javascripts/battle.js
@@ -0,0 +1,93 @@
+/**
+ * 戦闘ロジック
+ * @param {Object} spec
+ * @param {Object} my
+ */
+var battle = function(spec,my){
+    var that = {};
+    
+    that.MAX_ACTIVE = 5000;
+    that.ATACK_HIT = 1;
+    that.ATACK_MISS = 2;
+    that.ATACK_GUARD = 3;
+    that.ATACK_CRITICAL = 4;
+    
+    var statusArray = spec.statusArray;
+    var atackUserId = null;
+    
+    that.getStatusArray = function() {
+        return statusArray;
+    };
+    
+    /**
+     * ウェイトフェイズ実行 
+     * @return {Integer} 行動権を取得したプレイヤーID
+     */
+    that.doWaitPhase = function() {
+        while(true){
+            //アクティブゲージを加算
+            for(var uid in statusArray){
+                statusArray[uid].active += statusArray[uid].speed;
+            }
+            
+            //アクティブゲージが満タンか確認
+            for(var uid in statusArray){
+                if(statusArray[uid].active >= that.MAX_ACTIVE){
+                    atackUserId = uid;
+                    if(statusArray[uid].battery < 5) {
+                        statusArray[uid].battery ++;
+                    }
+                    
+                    return uid;
+                }
+            }
+        }
+    };
+    
+    /**
+     * 攻撃 
+     * @param {Object} command
+     */
+    that.atack = function(command) {
+            var hit = 0;
+            var atackBattery = command.atackBattery;
+            var defenthBattery = command.defenthBattery;
+            var damage = statusArray[atackUserId].weapons[atackBattery].power;
+            
+            if(defenthBattery === 0) {
+                damage = damage*2;
+                hit = that.ATACK_CRITICAL;
+            }else if(atackBattery > defenthBattery){
+                damage = damage;
+                hit = that.ATACK_HIT;
+            } else if(atackBattery === defenthBattery) {
+                damage = damage/2;
+                hit = that.ATACK_GUARD;
+            } else {
+                damage = 0;
+                hit = that.ATACK_MISS;
+            }
+
+            var defenthUserId = null;
+            for(var uid in statusArray) {
+                if(uid !== atackUserId) {
+                    defenthUserId = uid;
+                    break;
+                }
+            }
+            
+            statusArray[atackUserId].battery -= atackBattery;
+            statusArray[defenthUserId].hp -= damage;
+            statusArray[defenthUserId].battery -= defenthBattery;
+            
+            atackUserId = null;
+            
+            var ret = {
+                hit : hit,
+                damage : damage
+            };
+            return ret;
+    };
+    
+    return that;
+};
--- public/test/battleTest.js
+++ public/test/battleTest.js
@@ -1,7 +1,193 @@
-describe('Number', function() {
-    describe('calc', function() {
-        it('add', function() {
-            assert(4+8 == 12);
+describe('Battleクラスのテスト', function() {
+    describe('ウェイトフェイズ', function() {
+        it('素早いキャラクターのターンになる', function() {
+            var testData = {};
+            testData[1] = {
+                name : 'グランブレイバー',
+                pictName : 'GranBraver.PNG',
+                hp : 3200,
+                speed : 230,
+                active : 0,
+                battery : 5,
+                weapons : {
+                    1 : {name : 'バスターナックル',power : 800},
+                    2 : {name : 'バスターナックル',power : 1100},
+                    3 : {name : 'バスターナックル',power : 1600},
+                    4 : {name : 'バスターナックル',power : 2100},
+                    5 : {name : 'バスターナックル',power : 2800}
+                }
+            };
+            testData[2] = {
+                name : 'ランドーザ',
+                pictName : 'Landozer.PNG',
+                hp : 4700,
+                speed : 150,
+                active : 0,
+                battery : 5,
+                weapons : {
+                    1 : {name:'ブレイクパンチ',power:1200},
+                    2 : {name:'ブレイクパンチ',power:1700},
+                    3 : {name:'ブレイクパンチ',power:2300},
+                    4 : {name:'ブレイクパンチ',power:2900},
+                    5 : {name:'ブレイクパンチ',power:3800}
+                }
+            }; 
+            
+            var Battle = battle({
+                statusArray : testData    
+            });
+            
+            var ret = Battle.doWaitPhase();
+            var statusArray = Battle.getStatusArray();
+            assert.equal('1',ret);
+            assert.isTrue(statusArray[1].active>=Battle.MAX_ACTIVE,"アクティブゲージが最大値以上になる");
+        });
+        
+        it('素早さが同じ場合は、インデックスが若いキャラクターのターンになる',function(){
+            var testData = {};
+            testData[5] = {
+                name : 'グランブレイバー',
+                pictName : 'GranBraver.PNG',
+                hp : 3200,
+                speed : 230,
+                active : 0,
+                battery : 5,
+                weapons : {
+                    1 : {name : 'バスターナックル',power : 800},
+                    2 : {name : 'バスターナックル',power : 1100},
+                    3 : {name : 'バスターナックル',power : 1600},
+                    4 : {name : 'バスターナックル',power : 2100},
+                    5 : {name : 'バスターナックル',power : 2800}
+                }
+            };
+            testData[2] = {
+                name : 'ゼロブレイバー',
+                pictName : 'Landozer.PNG',
+                hp : 4700,
+                speed : 230,
+                active : 0,
+                battery : 5,
+                weapons : {
+                    1 : {name:'ブレイクパンチ',power:1200},
+                    2 : {name:'ブレイクパンチ',power:1700},
+                    3 : {name:'ブレイクパンチ',power:2300},
+                    4 : {name:'ブレイクパンチ',power:2900},
+                    5 : {name:'ブレイクパンチ',power:3800}
+                }
+            }; 
+            
+            var Battle = battle({
+                statusArray : testData    
+            });
+            
+            var ret = Battle.doWaitPhase();
+            var statusArray = Battle.getStatusArray();
+            assert.equal('2',ret);
+            assert.isTrue(statusArray[5].active>=Battle.MAX_ACTIVE,"アクティブゲージが最大値以上になる");
+            assert.isTrue(statusArray[2].active>=Battle.MAX_ACTIVE,"アクティブゲージが最大値以上になる");
+            
         });
     });
-});
+    
+    describe('攻撃', function() {
+        it('攻撃側のバッテリーが多いので攻撃がヒットする',function(){
+            var testData = {};
+            testData[1] = {
+                name : 'グランブレイバー',
+                pictName : 'GranBraver.PNG',
+                hp : 3200,
+                speed : 230,
+                active : 0,
+                battery : 5,
+                weapons : {
+                    1 : {name : 'バスターナックル',power : 800},
+                    2 : {name : 'バスターナックル',power : 1100},
+                    3 : {name : 'バスターナックル',power : 1600},
+                    4 : {name : 'バスターナックル',power : 2100},
+                    5 : {name : 'バスターナックル',power : 2800}
+                }
+            };
+            testData[2] = {
+                name : 'ランドーザ',
+                pictName : 'Landozer.PNG',
+                hp : 4700,
+                speed : 150,
+                active : 0,
+                battery : 5,
+                weapons : {
+                    1 : {name:'ブレイクパンチ',power:1200},
+                    2 : {name:'ブレイクパンチ',power:1700},
+                    3 : {name:'ブレイクパンチ',power:2300},
+                    4 : {name:'ブレイクパンチ',power:2900},
+                    5 : {name:'ブレイクパンチ',power:3800}
+                }
+            }; 
+            
+            var Battle = battle({
+                statusArray : testData    
+            });
+            
+            var retWaitPhase = Battle.doWaitPhase();
+            var retAtack = Battle.atack({
+                atackBattery : 3,
+                defenthBattery : 1
+            });
+            var statusArray = Battle.getStatusArray();
+            assert.equal('1',retWaitPhase);
+            assert.equal(Battle.ATACK_HIT,retAtack.hit);
+            assert.equal(1600,retAtack.damage);
+            assert.equal(2,statusArray[1].battery);
+            assert.equal(4,statusArray[2].battery);
+        });
+        
+         it('攻撃・防御側が同じバッテリーを出したので防御判定になりダメージが半減される',function(){
+            var testData = {};
+            testData[1] = {
+                name : 'グランブレイバー',
+                pictName : 'GranBraver.PNG',
+                hp : 3200,
+                speed : 230,
+                active : 0,
+                battery : 5,
+                weapons : {
+                    1 : {name : 'バスターナックル',power : 800},
+                    2 : {name : 'バスターナックル',power : 1100},
+                    3 : {name : 'バスターナックル',power : 1600},
+                    4 : {name : 'バスターナックル',power : 2100},
+                    5 : {name : 'バスターナックル',power : 2800}
+                }
+            };
+            testData[2] = {
+                name : 'ランドーザ',
+                pictName : 'Landozer.PNG',
+                hp : 4700,
+                speed : 150,
+                active : 0,
+                battery : 5,
+                weapons : {
+                    1 : {name:'ブレイクパンチ',power:1200},
+                    2 : {name:'ブレイクパンチ',power:1700},
+                    3 : {name:'ブレイクパンチ',power:2300},
+                    4 : {name:'ブレイクパンチ',power:2900},
+                    5 : {name:'ブレイクパンチ',power:3800}
+                }
+            }; 
+            
+            var Battle = battle({
+                statusArray : testData    
+            });
+            
+            var retWaitPhase = Battle.doWaitPhase();
+            var retAtack = Battle.atack({
+                atackBattery : 4,
+                defenthBattery : 4
+            });
+            var statusArray = Battle.getStatusArray();
+            assert.isTrue('1'===retWaitPhase,'ユーザID1のターンになる');
+            assert.isTrue(Battle.ATACK_GUARD===retAtack.hit,'防御判定になる');
+            assert.isTrue(2100/2===retAtack.damage,'ダメージが半減される');
+            assert.isTrue(1===statusArray[1].battery,'攻撃側バッテリーが減る');
+            assert.isTrue(1===statusArray[2].battery,'防御側バッテリーが減る');
+        });
+    });
+}); 
