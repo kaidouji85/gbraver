8e80a2b98c8a2d00100d3634a32c97902b2b904c
竹内佑介 <yusuke@takeuchiyuusuke-no-MacBook-Air.local>
2014-02-08 21:38:03 +0900

    先ほどのプッシュしたものは間違っていたので、正しいものをあげる

--- test/serverTest.js
+++ test/serverTest.js
@@ -1,17 +1,17 @@
-var assert = require('chai').assert;
-var io = require('socket.io-client');
-const SERVER_PORT = 3000;
-
 describe('serverクラスのテスト', function(){
+    var assert = require('chai').assert;
+    var io = require('socket.io-client');
+    var SERVER_PORT = 3000;
+    var SERVER_URL = 'http://localhost';
     var roomId = 0;
     var option = {
         'force new connection' : true,
          port : 3000
     };
     var app = require('http').createServer().listen(3000);
-    var httpServer = require('../server.js');
+    var server = require('../server.js');
     var Server = server({
-        httpServer : httpServer
+        httpServer : app
     });
 
     afterEach(function(){
@@ -20,7 +20,7 @@ describe('serverクラスのテスト', function(){
 
     describe('入室正常系',function(){
         it('入室したらサーバから「succesEnterRoom」が返される',function(done){
-            var client = io.connect('http://localhost',option);
+            var client = io.connect(SERVER_URL,option);
             client.emit('enterRoom',{
                 roomId : roomId,
                 userId : 0
@@ -31,12 +31,13 @@ describe('serverクラスのテスト', function(){
             });
         });
         
+        //本システムのsocket.ioサーバテストは、2つのクライアントデータが正しく
         var userIds = [0,1];
         userIds.forEach(function(userId){
             it('2人入室したらサーバから「gameStart」が返される #ユーザ' + userId + 'の確認', function(done) {
                 var clients = [];
                 for (var i in userIds) {
-                    clients[i] = io.connect('http://localhost', option);
+                    clients[i] = io.connect(SERVER_URL, option);
                     clients[i].emit('enterRoom', {
                         roomId : roomId,
                         userId : i
