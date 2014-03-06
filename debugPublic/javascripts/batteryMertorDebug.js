bf2c4d713fcd1eddc3cf613947ddc32a5dc34e2f
竹内佑介 <yusuke@takeuchiyuusuke-no-MacBook-Air.local>
2014-03-07 07:17:15 +0900

    バッテリー表示クラスを作成した。

--- debugPublic/javascripts/batteryMertorDebug.js
+++ debugPublic/javascripts/batteryMertorDebug.js
@@ -1,16 +1,35 @@
 var PICT_PREFIX = location.origin + '/images/';
+var PICT_BATTERY_GAUGE = 'batteryGauge.png';
+var PICT_BATTERY_BACK = 'batteryBack.png';
 var core;
 enchant();
 window.onload = function() {
     core = new Core(320, 320);
     core.fps = 60;
     core.rootScene.backgroundColor = "black";
-    //core.preload();
+    core.preload(PICT_PREFIX+PICT_BATTERY_GAUGE);
+    core.preload(PICT_PREFIX+PICT_BATTERY_BACK);
     core.start();
     core.onload = function(){
-        var BatteryMertor = batteryMertor();
-        BatteryMertor.x = 10;
-        BatteryMertor.x = 30;
-        core.rootScene.addChild(BatteryMertor);
+        var BatteryMertorR = batteryMertor({
+            gaugeImage : core.assets[PICT_PREFIX + PICT_BATTERY_GAUGE],
+            backImage : core.assets[PICT_PREFIX + PICT_BATTERY_BACK],
+            direction : 'right'
+        });
+        BatteryMertorR.x = 40;
+        BatteryMertorR.y = 40;
+        BatteryMertorR.setValue(4);
+        core.rootScene.addChild(BatteryMertorR);
+        
+        var BatteryMertorL = batteryMertor({
+            gaugeImage : core.assets[PICT_PREFIX + PICT_BATTERY_GAUGE],
+            backImage : core.assets[PICT_PREFIX + PICT_BATTERY_BACK],
+            direction : 'left'
+        });
+        BatteryMertorL.x = 40;
+        BatteryMertorL.y = 80;
+        BatteryMertorL.setValue(4);
+        core.rootScene.addChild(BatteryMertorL);        
+        
     };
 };
--- public/javascripts/batteryMertor.js
+++ public/javascripts/batteryMertor.js
@@ -1,4 +1,32 @@
 function batteryMertor(spec,my){
-    var that = new Sprite(32,32);
+    var WIDTH = 24;
+    var HEIGHT = 24;
+    var BACK_WIDTH = 120;
+    var MAX_BATTERY = 5;
+    
+    var that = new Group();
+    var gaugeImage = spec.gaugeImage;
+    var backImage = spec.backImage;
+    var direction = spec.direction;
+    var batteryArray = [];
+    var batteryBack = new Sprite(BACK_WIDTH,HEIGHT);
+    
+    batteryBack.image = backImage;
+    that.addChild(batteryBack);
+    
+    for(var i=0; i<MAX_BATTERY; i++){
+        batteryArray[i] = new Sprite(WIDTH,HEIGHT);
+        batteryArray[i].image = gaugeImage;
+        batteryArray[i].x = direction==='right'?WIDTH*i:WIDTH*(MAX_BATTERY-1)-WIDTH*i;
+        that.addChild(batteryArray[i]);
+    }
+    
+    that.setValue = function(value){
+        for(var i=0; i<MAX_BATTERY; i++){
+            batteryArray[i].visible = i<value ? true : false;
+        }
+
+    };
+    
     return that;
 }
