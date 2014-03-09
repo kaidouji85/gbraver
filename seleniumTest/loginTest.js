e1d31235bf36629a5245469a93d05c71c5548628
竹内佑介 <yusuke@takeuchiyuusuke-no-MacBook-Air.local>
2014-03-09 20:22:13 +0900

    seleniumテストで2人ログインのケースを削除した。

--- seleniumTest/loginTest.js
+++ seleniumTest/loginTest.js
@@ -4,19 +4,14 @@ var test = require('selenium-webdriver/testing');
 var URL = 'localhost:3000';
 
 test.describe('ログイン', function() {
-    var drivers = new Array();
-    
+    var driver;
     test.before(function() {
-        for(var i=0; i<2; i++){
-            var driver = new webdriver.Builder().
-                           withCapabilities(webdriver.Capabilities.chrome()).
-                           build();
-            drivers.push(driver);
-        }
+        driver = new webdriver.Builder().
+                     withCapabilities(webdriver.Capabilities.chrome()).
+                     build();
     });
     
     test.it('ログインするとルームID、ユーザIDがmetaタグに格納される', function(){
-        var driver = drivers[0];
         driver.get(URL);
         driver.findElement(webdriver.By.name('userId')).sendKeys(1);
         driver.findElement(webdriver.By.name('roomId')).sendKeys(0);
@@ -35,42 +30,8 @@ test.describe('ログイン', function() {
         });
                 
     });
-    
-    test.it('異なるユーザが2人同じ部屋にログインしたので「succesEnterRoom」がサーバから送信される', function() {
-        //ログイン処理
-        drivers.forEach(function(driver,index) {
-            driver.get(URL);
-            var userId = driver.findElement(webdriver.By.name('userId'));
-            var roomId = driver.findElement(webdriver.By.name('roomId'));
-            var loginButton = driver.findElement(webdriver.By.name('loginButton'));
-            userId.sendKeys(index===0 ? 1 : 2);
-            roomId.sendKeys(1);
-            loginButton.click();
-        });
-        
-        //入室確認
-        drivers.forEach(function(driver,index) {
-            driver.wait(function() {
-                return driver.getTitle().then(function(title) {
-                    return 'Gブレイバー 戦闘'===title;
-                });
-            }, 1000);
-            
-            /*
-            driver.executeScript(function() {
-                gbraver.acceptGameStart = false;
-                gbraver.socket.once("gameStart",function(data){
-                    gbraver.acceptGameStart = true;
-                    gbraver.gameStartRespData;
-                });
-            });
-            */
-        });
-    }); 
 
     test.after(function() {
-        drivers.forEach(function(driver){
-            driver.quit();
-        });
+        driver.quit();
     });
 }); 
