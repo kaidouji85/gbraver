c13a58ef39f1d01a266097b0ccb55d444967cc2a
竹内佑介 <yusuke@takeuchiyuusuke-no-MacBook-Air.local>
2014-03-28 23:49:06 +0900

    mongoDbTest.jsでDBへのデータ挿入処理を別ファイル化した。

--- mongoDbTest/getUserDataTest.js
+++ mongoDbTest/getUserDataTest.js
@@ -1,17 +1,16 @@
 var assert = require('chai').assert;
 var mongoDao = require('../mongoDao.js');
-var MongoClient = require('mongodb').MongoClient;
+var insertData = require('./testData.js').insertData;
 
 describe('Mongo DBからユーザ情報を取得する', function() {
     const mongoUrl = 'mongodb://localhost/gbraverTest';
 
     before(function(done) {
-        insertData(function(err, result) {
+        insertData(mongoUrl,function(err, result) {
             done();
         });
     });
 
-    //TODO : テスト実行時にDBデータを入れ替える処理が未実装
     it('存在するユーザの情報を取得したのでユーザ情報を取得することができる', function(done) {
         var dao = mongoDao({
             url : mongoUrl
@@ -55,85 +54,5 @@ describe('Mongo DBからユーザ情報を取得する', function() {
             };
             assert.deepEqual(userData, expect, '正しいユーザ情報を取得出来た');
         }
-
     });
-
-    function insertData(fnc) {
-        MongoClient.connect(mongoUrl, function(err, db) {
-            var collection = db.collection('users');
-            collection.remove({}, {}, function(err, deletes) {
-                var take = {
-                    userId : 'take',
-                    password : 'pass',
-                    status : {
-                        name : 'グランブレイバー',
-                        pictName : 'GranBraver.PNG',
-                        hp : 3200,
-                        speed : 230,
-                        weapons : {
-                            1 : {
-                                name : 'バスターナックル',
-                                power : 800
-                            },
-                            2 : {
-                                name : 'バスターナックル',
-                                power : 1100
-                            },
-                            3 : {
-                                name : 'バスターナックル',
-                                power : 1600
-                            },
-                            4 : {
-                                name : 'バスターナックル',
-                                power : 2100
-                            },
-                            5 : {
-                                name : 'バスターナックル',
-                                power : 2800
-                            },
-                        }
-                    }
-                };
-
-                var uchi = {
-                    userId : 'uchi',
-                    password : 'pass',
-                    status : {
-                        name : 'ランドーザ',
-                        pictName : 'Landozer.PNG',
-                        hp : 4700,
-                        speed : 150,
-                        weapons : {
-                            1 : {
-                                name : 'ブレイクパンチ',
-                                power : 1200
-                            },
-                            2 : {
-                                name : 'ブレイクパンチ',
-                                power : 1700
-                            },
-                            3 : {
-                                name : 'ブレイクパンチ',
-                                power : 2300
-                            },
-                            4 : {
-                                name : 'ブレイクパンチ',
-                                power : 2900
-                            },
-                            5 : {
-                                name : 'ブレイクパンチ',
-                                power : 3800
-                            }
-                        }
-                    }
-                };
-                
-                collection.insert([take,uchi], function(err, data) {
-                    db.close();
-                    fnc(null,true);
-                });
-            });
-        });
-    }
-
 });
---
+++ mongoDbTest/testData.js
@@ -0,0 +1,83 @@
+var MongoClient = require('mongodb').MongoClient;
+
+var take = {
+    userId : 'take',
+    password : 'pass',
+    status : {
+        name : 'グランブレイバー',
+        pictName : 'GranBraver.PNG',
+        hp : 3200,
+        speed : 230,
+        weapons : {
+            1 : {
+                name : 'バスターナックル',
+                power : 800
+            },
+            2 : {
+                name : 'バスターナックル',
+                power : 1100
+            },
+            3 : {
+                name : 'バスターナックル',
+                power : 1600
+            },
+            4 : {
+                name : 'バスターナックル',
+                power : 2100
+            },
+            5 : {
+                name : 'バスターナックル',
+                power : 2800
+            },
+        }
+    }
+};
+
+var uchi = {
+    userId : 'uchi',
+    password : 'pass',
+    status : {
+        name : 'ランドーザ',
+        pictName : 'Landozer.PNG',
+        hp : 4700,
+        speed : 150,
+        weapons : {
+            1 : {
+                name : 'ブレイクパンチ',
+                power : 1200
+            },
+            2 : {
+                name : 'ブレイクパンチ',
+                power : 1700
+            },
+            3 : {
+                name : 'ブレイクパンチ',
+                power : 2300
+            },
+            4 : {
+                name : 'ブレイクパンチ',
+                power : 2900
+            },
+            5 : {
+                name : 'ブレイクパンチ',
+                power : 3800
+            }
+        }
+    }
+};
+
+var testData = [take,uchi];
+
+function insertData(mongoUrl,fnc) {
+    MongoClient.connect(mongoUrl, function(err, db) {
+        var collection = db.collection('users');
+        collection.remove({}, {}, function(err, deletes) {
+            collection.insert(testData, function(err, data) {
+                db.close();
+                fnc(null, true);
+            });
+        });
+    });
+}
+
+module.exports.insertData = insertData;
