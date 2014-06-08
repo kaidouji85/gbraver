a5343a2f1bac0ff2a5e539b54fe23cbb1e99df12
kaidouji85 <kaidouji85@gmail.com>
2014-06-09 07:56:53 +0900

    サーバ側テストコードをsocket.io1.0対応した。

--- unitTest/serverSetArmdozerTest.js
+++ unitTest/serverSetArmdozerTest.js
@@ -1,6 +1,7 @@
+//TODO : socket.ioコネクション処理を1.0推奨の非同期方式にする
 describe('serverクラスのテスト',function(){
     var SERVER_PORT = 3003;
-    var SERVER_URL = 'http://localhost';
+    var SERVER_URL = 'http://localhost:'+SERVER_PORT;
     
     var testPlayerData = require('./testPlayerData.js');
     var assert = require('chai').assert;
@@ -22,8 +23,7 @@ describe('serverクラスのテスト',function(){
     
     before(function(){
         option = {
-            'force new connection' : true,
-            port : SERVER_PORT
+            'forceNew' : true
         };
         Server = server({
             httpServer : app
