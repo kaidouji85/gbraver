5583c64b37b013afb915a2d6104c351252ffebb7
kaidouji85 <kaidouji85@gmail.com>
2014-07-06 23:08:49 +0900

    gameクラスをgameBaseから継承するようにした。

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -2,36 +2,10 @@ function game(spec, my) {
     /**
      * ゲームコア
      */
-    var core = new Core(320, 480);
+    var core = gameBase();
     var userId = spec.userId;
     var emitChangeScene = function(scene){};
     var emitSendMessage = function(message,data){};
-    
-    core.PICT_PREFIX = location.origin + '/images/';
-    core.PICT_ACTIVE_BAR = core.PICT_PREFIX+'activeBar.png';
-    core.PICT_ACTIVE_BAR_BACK = core.PICT_PREFIX+'activeBack.png';
-    core.PICT_BATTERY_GAUGE = core.PICT_PREFIX+'batteryGauge.png';
-    core.PICT_BATTERY_BACK = core.PICT_PREFIX+'batteryBack.png';
-    core.PICT_BATTERY_NUMBER = core.PICT_PREFIX+'batteryNumber.png';
-    core.PICT_ATTTACK_PARTICLE = core.PICT_PREFIX+'attackParticle.png';
-        
-    core.fps = 60;
-    core.battleScene = null;
-    core.roomSelectScene = null;
-    core.setArmdozerScene = null;
-    preLoad();
-    
-    function preLoad() {
-        //戦闘画面
-        core.preload(core.PICT_PREFIX+'GranBraver.PNG');
-        core.preload(core.PICT_PREFIX+'Landozer.PNG');
-        core.preload(core.PICT_ACTIVE_BAR);
-        core.preload(core.PICT_ACTIVE_BAR_BACK);
-        core.preload(core.PICT_BATTERY_GAUGE);
-        core.preload(core.PICT_BATTERY_BACK);
-        core.preload(core.PICT_BATTERY_NUMBER);
-        core.preload(core.PICT_ATTTACK_PARTICLE);
-    }
 
     /**
      * 戦闘シーンに切り替える 
