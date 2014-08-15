ce48f728f7371d7227b17861791721a351658ebc
kaidouji85 <kaidouji85@gmail.com>
2014-08-16 01:01:50 +0900

    ボタンを押した時に色調が暗くなるようにした。

--- public/javascripts/pictButton.js
+++ public/javascripts/pictButton.js
@@ -2,16 +2,30 @@ function pictButton(spec,my){
     var that = new Group();
     var text = spec.text;
     var pict = spec.pict;
-    var buttonSprite = new Sprite(128,32);
+    var COLOR_WHITE = '#FFFFFF';
+    var COLOR_GRAY = '#708090';
+    var BUTTON_WIDTH = 128;
+    var BUTTON_HEIGHT = 32;
+    var buttonSprite = new Sprite(BUTTON_WIDTH,BUTTON_HEIGHT);
     var buttonLabel = new Label(text);
 
     buttonSprite.image = pict;
     that.addChild(buttonSprite);
 
-    buttonLabel.color = 'white';
-    buttonLabel.x = (128 - buttonLabel._boundWidth)/2;
-    buttonLabel.y = 10;
+    buttonLabel.color = COLOR_WHITE;
+    buttonLabel.x = (BUTTON_WIDTH - buttonLabel._boundWidth)/2;
+    buttonLabel.y = (BUTTON_HEIGHT - buttonLabel._boundHeight)/2;
     that.addChild(buttonLabel);
+
+    that.addEventListener(Event.TOUCH_START,function(){
+        buttonLabel.color = COLOR_GRAY;
+        buttonSprite.frame = 1;
+    });
+
+    that.addEventListener(Event.TOUCH_END,function(){
+        buttonLabel.color = COLOR_WHITE;
+        buttonSprite.frame = 0;
+    });
 
     return that;
 }
