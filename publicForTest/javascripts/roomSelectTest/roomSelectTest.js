a4316bd920184f89464455fb3a4f161eb56a8059
kaidouji85 <kaidouji85@gmail.com>
2014-08-19 07:10:47 +0900

    ルームセレクト画面テストにボタン非表示チェック追加。

--- publicForTest/javascripts/roomSelectTest/roomSelectTest.js
+++ publicForTest/javascripts/roomSelectTest/roomSelectTest.js
@@ -16,19 +16,25 @@ function enterRoomAndGameStart(){
             enterRoom();
         };
     }
-    
+
     function enterRoom(){
         //ルーム2を押す
         touch(Game.roomSelectScene.enterRoomButtonArray[2]);
         Game.onSendMessage(assertEnterRoom);
     }
-    
+
     function assertEnterRoom(message,data){
         var expect = {
             roomId : 2
         };
         assert.equal(message, 'enterRoom', '入室処理のサーバ送信メッセージ名が正しい');
         assert.deepEqual(expect, data, '入室処理のサーバ送信データが正しい');
+        assert.equal(Game.currentScene.enterRoomButtonArray[0].getVisible(),false,'ルーム0ボタンが非表示');
+        assert.equal(Game.currentScene.enterRoomButtonArray[0].getVisible(),false,'ルーム1ボタンが非表示');
+        assert.equal(Game.currentScene.enterRoomButtonArray[0].getVisible(),false,'ルーム2ボタンが非表示');
+        assert.equal(Game.currentScene.enterRoomButtonArray[0].getVisible(),false,'ルーム3ボタンが非表示');
+        assert.equal(Game.currentScene.enterRoomButtonArray[0].getVisible(),false,'ルーム4ボタンが非表示');
+        assert.equal(Game.currentScene.prevButton.getVisible(),false,'戻るボタンが非表示');
         finishTest();
     }
 }
