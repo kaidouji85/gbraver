63d63e2aa09af7719a876ac396a967e934bd1718
kaidouji85 <kaidouji85@gmail.com>
2014-08-20 21:57:29 +0900

    roomSelectToBattleTest.jsのリファクタリング。

--- publicForTest/javascripts/changeSceneTest/roomSelectToBattleTest.js
+++ publicForTest/javascripts/changeSceneTest/roomSelectToBattleTest.js
@@ -3,75 +3,6 @@ window.onload = roomSelectToBattle;
 
 function roomSelectToBattle(){
     var assert = chai.assert;
-    //TODO : データ構造が気持ち悪い。ユーザIDが重複しまくってる。サーバ側の修正も必要だから一筋縄ではいかない。
-    var gameStartData = {
-        'test001@gmail.com' : {
-            userId : 'test001@gmail.com',
-            status : {
-                name : 'グランブレイバー',
-                pictName : 'GranBraver.PNG',
-                active : 0,
-                battery : 5,
-                hp : 3200,
-                speed : 500,
-                weapons : {
-                    1 : {
-                        name : 'バスターナックル',
-                        power : 800
-                    },
-                    2 : {
-                        name : 'バスターナックル',
-                        power : 1100
-                    },
-                    3 : {
-                        name : 'バスターナックル',
-                        power : 1600
-                    },
-                    4 : {
-                        name : 'バスターナックル',
-                        power : 2100
-                    },
-                    5 : {
-                        name : 'バスターナックル',
-                        power : 2800
-                    },
-                }
-            }
-        },
-        'test002@gmail.com' : {
-            userId : 'test002@gmail.com',
-            status : {
-                name : 'ランドーザ',
-                pictName : 'Landozer.PNG',
-                hp : 4700,
-                speed : 300,
-                active : 0,
-                battery : 5,
-                weapons : {
-                    1 : {
-                        name : 'ブレイクパンチ',
-                        power : 1200
-                    },
-                    2 : {
-                        name : 'ブレイクパンチ',
-                        power : 1700
-                    },
-                    3 : {
-                        name : 'ブレイクパンチ',
-                        power : 2300
-                    },
-                    4 : {
-                        name : 'ブレイクパンチ',
-                        power : 2900
-                    },
-                    5 : {
-                        name : 'ブレイクパンチ',
-                        power : 3800
-                    }
-                }
-            }
-        }
-    };
     var Game;
     initGame();
 
@@ -94,7 +25,79 @@ function roomSelectToBattle(){
         };
         assert.equal(message,'enterRoom','メッセージが正しい');
         assert.deepEqual(data,expectData,'入室メッセージのパラメータが正しい');
+        gameStart();
+    }
 
+    function gameStart() {
+        //TODO : データ構造が気持ち悪い。ユーザIDが重複しまくってる。サーバ側の修正も必要だから一筋縄ではいかない。
+        var gameStartData = {
+            'test001@gmail.com' : {
+                userId : 'test001@gmail.com',
+                status : {
+                    name : 'グランブレイバー',
+                    pictName : 'GranBraver.PNG',
+                    active : 0,
+                    battery : 5,
+                    hp : 3200,
+                    speed : 500,
+                    weapons : {
+                        1 : {
+                            name : 'バスターナックル',
+                            power : 800
+                        },
+                        2 : {
+                            name : 'バスターナックル',
+                            power : 1100
+                        },
+                        3 : {
+                            name : 'バスターナックル',
+                            power : 1600
+                        },
+                        4 : {
+                            name : 'バスターナックル',
+                            power : 2100
+                        },
+                        5 : {
+                            name : 'バスターナックル',
+                            power : 2800
+                        },
+                    }
+                }
+            },
+            'test002@gmail.com' : {
+                userId : 'test002@gmail.com',
+                status : {
+                    name : 'ランドーザ',
+                    pictName : 'Landozer.PNG',
+                    hp : 4700,
+                    speed : 300,
+                    active : 0,
+                    battery : 5,
+                    weapons : {
+                        1 : {
+                            name : 'ブレイクパンチ',
+                            power : 1200
+                        },
+                        2 : {
+                            name : 'ブレイクパンチ',
+                            power : 1700
+                        },
+                        3 : {
+                            name : 'ブレイクパンチ',
+                            power : 2300
+                        },
+                        4 : {
+                            name : 'ブレイクパンチ',
+                            power : 2900
+                        },
+                        5 : {
+                            name : 'ブレイクパンチ',
+                            power : 3800
+                        }
+                    }
+                }
+            }
+        };
         Game.onChangeScene(assertChangeScene);
         Game.emitServerResp('gameStart',gameStartData);
     }
