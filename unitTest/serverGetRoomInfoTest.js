f167a857f181170a44c7901764f4e332e44c6e01
kaidouji85 <kaidouji85@gmail.com>
2014-08-28 07:09:46 +0900

    ルーム情報取得テストのパターン増加。

--- unitTest/serverGetRoomInfoTest.js
+++ unitTest/serverGetRoomInfoTest.js
@@ -28,7 +28,7 @@ describe('serverクラスのテスト', function() {
     });
 
     describe('ルーム情報取得',function(){
-        it('誰も入室していない',function(done){
+        it('全ルームに誰も入室していない',function(done){
             var client = io(SERVER_URL, option);
             client.on('connect',doAuth);
 
@@ -56,5 +56,147 @@ describe('serverクラスのテスト', function() {
                 done();
             }
         });
+
+        it('ルーム3に1人入室',function(done){
+            var client;
+
+            enterRoomByTest002();
+            function enterRoomByTest002(){
+                enterRoom('test002@gmail.com',3,connectByTest001);
+            }
+
+            function connectByTest001() {
+                client = io(SERVER_URL, option);
+                client.on('connect',doAuth);
+            }
+
+            function doAuth() {
+                client.emit('auth',{
+                    userId : 'test001@gmail.com'
+                });
+                client.once('successAuth',getRoomInfo);
+            }
+
+            function getRoomInfo(){
+                client.emit('getRoomInfo');
+                client.once('successGetRoomInfo',assertGetRoomInfo);
+            }
+
+            function assertGetRoomInfo(data) {
+                var expect = {
+                    '0' : [],
+                    '1' : [],
+                    '2' : [],
+                    '3' : ['test002@gmail.com'],
+                    '4' : []
+                };
+                assert.deepEqual(data,expect,'ルーム情報が正しい');
+                done();
+            }
+        });
+
+        it('ルーム1に2人入室',function(done){
+            var client;
+
+            enterRoomByTest002();
+            function enterRoomByTest002(){
+                enterRoom('test002@gmail.com',1,enterRoomByTest003);
+            }
+
+            function enterRoomByTest003() {
+                enterRoom('test003@gmail.com',1,connectByTest001);
+            }
+
+            function connectByTest001() {
+                client = io(SERVER_URL, option);
+                client.on('connect',doAuth);
+            }
+
+            function doAuth() {
+                client.emit('auth',{
+                    userId : 'test001@gmail.com'
+                });
+                client.once('successAuth',getRoomInfo);
+            }
+
+            function getRoomInfo(){
+                client.emit('getRoomInfo');
+                client.once('successGetRoomInfo',assertGetRoomInfo);
+            }
+
+            function assertGetRoomInfo(data) {
+                var expect = {
+                    '0' : [],
+                    '1' : ['test002@gmail.com','test003@gmail.com'],
+                    '2' : [],
+                    '3' : [],
+                    '4' : []
+                };
+                assert.deepEqual(data,expect,'ルーム情報が正しい');
+                done();
+            }
+        });
+
+        it('ルーム1、2に一人ずつ入室',function(done){
+            var client;
+
+            enterRoomByTest002();
+            function enterRoomByTest002(){
+                enterRoom('test002@gmail.com',1,enterRoomByTest003);
+            }
+
+            function enterRoomByTest003() {
+                enterRoom('test003@gmail.com',2,connectByTest001);
+            }
+
+            function connectByTest001() {
+                client = io(SERVER_URL, option);
+                client.on('connect',doAuth);
+            }
+
+            function doAuth() {
+                client.emit('auth',{
+                    userId : 'test001@gmail.com'
+                });
+                client.once('successAuth',getRoomInfo);
+            }
+
+            function getRoomInfo(){
+                client.emit('getRoomInfo');
+                client.once('successGetRoomInfo',assertGetRoomInfo);
+            }
+
+            function assertGetRoomInfo(data) {
+                var expect = {
+                    '0' : [],
+                    '1' : ['test002@gmail.com',],
+                    '2' : ['test003@gmail.com'],
+                    '3' : [],
+                    '4' : []
+                };
+                assert.deepEqual(data,expect,'ルーム情報が正しい');
+                done();
+            }
+        });
+
+
+        function enterRoom(userId, roomId, fn){
+            var client = io(SERVER_URL, option);
+            client.on('connect',doAuth);
+
+            function doAuth() {
+                client.emit('auth',{
+                    userId : userId
+                });
+                client.once('successAuth',enter);
+            }
+
+            function enter(){
+                client.emit('enterRoom',{
+                    roomId : roomId
+                });
+                client.once('succesEnterRoom',fn);
+            }
+        }
     });
 });
