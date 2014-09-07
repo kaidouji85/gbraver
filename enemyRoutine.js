70efa03c7586a1f79e91470773cd52c6f5d0be0e
kaidouji85 <kaidouji85@gmail.com>
2014-09-08 07:54:31 +0900

    敵CPUの攻撃ルーチンを実装した。

--- enemyRoutine.js
+++ enemyRoutine.js
@@ -1,6 +1,8 @@
 function enemyRoutine(spec,my){
     var that = {};
+    var NONE_PLAYER_CHARACTER = 'nonePlayerCharacter';
     var respData = null;
+    var attackUserId = null;
 
     that.setRespData = function(data){
         respData = data;
@@ -14,9 +16,17 @@ function enemyRoutine(spec,my){
 
         if(respData===null){
             command.method = 'ready';
-        } else if(respData.phase === 'defenthCommand') {
+        } else if(respData.phase === 'wait') {
+            attackUserId = respData.atackUserId;
+        } else if(respData.phase === 'defenthCommand' && attackUserId !== NONE_PLAYER_CHARACTER) {
             //TODO : ここに思考ルーチンを追加する
             command.method = 'defenth';
+            command.param = {
+                battery : 0
+            };
+        } else if(respData.phase === 'atackCommand' && attackUserId === NONE_PLAYER_CHARACTER) {
+            //TODO : ここに思考ルーチンを追加する
+            command.method = 'atack';
             command.param = {
                 battery : 0
             };
---
+++ unitTest/serverSinglePlayForEnemyAttack.js
@@ -0,0 +1,228 @@
+//TODO : socket.ioコネクション処理を1.0推奨の非同期方式にする
+describe('serverクラスのテスト', function() {
+    var SERVER_PORT = 3000;
+    var SERVER_URL = 'http://localhost:'+SERVER_PORT;
+
+    var testPlayerData = require('./testPlayerData.js');
+    var assert = require('chai').assert;
+    var io = require('socket.io-client');
+    var http = require('http');
+
+    var app;
+    var server = require('../server.js');
+    var testServer;
+    var option = {
+        'forceNew' : true
+    };
+
+    beforeEach(function() {
+        app = http.createServer().listen(SERVER_PORT);
+        testServer = server({
+            httpServer : app
+        });
+        testServer.onGetPlayerData(testPlayerData.getPlayerData);
+        testServer.onGetCharacterInfo(function(armdozerId,fn){
+            var landozer = {
+                name : 'グランブレイバー',
+                pictName : 'GranBraver.PNG',
+                hp : 3200,
+                speed : 500,
+                weapons : {
+                    1 : {
+                        name : 'バスターナックル',
+                        power : 800
+                    },
+                    2 : {
+                        name : 'バスターナックル',
+                        power : 1100
+                    },
+                    3 : {
+                        name : 'バスターナックル',
+                        power : 1600
+                    },
+                    4 : {
+                        name : 'バスターナックル',
+                        power : 2100
+                    },
+                    5 : {
+                        name : 'バスターナックル',
+                        power : 2800
+                    }
+                }
+            };
+            fn(null,landozer);
+        });
+    });
+
+    afterEach(function() {
+        app.close();
+    });
+
+    describe(' 一人用モード戦闘',function(){
+        it('敵が攻撃する',function(done){
+            var client = io(SERVER_URL, option);
+            client.on('connect',doAuth);
+
+            function doAuth() {
+                client.emit('auth',{
+                    userId : 'test002@gmail.com'
+                });
+                client.once('successAuth',startSinglePlay);
+            }
+
+            function startSinglePlay() {
+                client.emit('startSinglePlay',{
+                    enemyId : 'granBraver'
+                });
+                client.once('gameStart',gameStart);
+            }
+
+            function gameStart(){
+                client.emit('command',{
+                    method : 'ready'
+                });
+                client.once('resp',waitPhase);
+            }
+
+            function waitPhase(data){
+                var expect = {
+                    phase : 'wait',
+                    atackUserId : 'nonePlayerCharacter',
+                    turn : 10,
+                    statusArray : {
+                        'nonePlayerCharacter' : {
+                            hp : 3200,
+                            battery : 5,
+                            active : 5000
+                        },
+                        'test002@gmail.com' : {
+                            hp : 4700,
+                            battery : 5,
+                            active : 3000
+                        }
+                    }
+                };
+                assert.deepEqual(data,expect,'ウェイトフェイズの情報が正しい');
+                sendWaitPhaseCommand();
+            }
+
+            function sendWaitPhaseCommand(){
+                client.emit('command',{
+                    method : 'ok'
+                });
+                client.once('resp',attackCommandPhase);
+            }
+
+            function attackCommandPhase(data) {
+                var expect = {
+                    phase : 'atackCommand',
+                    statusArray : {
+                        'nonePlayerCharacter' : {
+                            hp : 3200,
+                            battery : 5,
+                            active : 5000
+                        },
+                        'test002@gmail.com' : {
+                            hp : 4700,
+                            battery : 5,
+                            active : 3000
+                        }
+                    }
+                };
+                assert.deepEqual(expect,data,'アタックフェイズのデータが正しい');
+                sendAtacckCommand();
+            }
+
+            function sendAtacckCommand() {
+                client.emit('command',{
+                    method : 'ok'
+                });
+                client.once('resp',defenseCommandPhase);
+            }
+
+            function defenseCommandPhase(data){
+                var expect = {
+                    phase : 'defenthCommand',
+                    statusArray : {
+                        'nonePlayerCharacter' : {
+                            hp : 3200,
+                            battery : 5,
+                            active : 5000
+                        },
+                        'test002@gmail.com' : {
+                            hp : 4700,
+                            battery : 5,
+                            active : 3000
+                        }
+                    }
+                };
+                assert.deepEqual(data,expect,'ディフェンスコマンドフェイズのデータが正しい');
+                sendDefenseCommand();
+            }
+
+            function sendDefenseCommand(){
+                client.emit('command',{
+                    method : 'defenth',
+                    param : {
+                        battery : 2
+                    }
+                });
+                client.once('resp',damagePhase);
+            }
+
+            function damagePhase(data){
+                var expect = {
+                    phase : 'damage',
+                    hit : 2,
+                    damage : 0,
+                    atackBattery : 0,
+                    defenthBattery : 2,
+                    statusArray : {
+                        'nonePlayerCharacter' : {
+                            hp : 3200,
+                            battery : 5,
+                            active : 0
+                        },
+                        'test002@gmail.com' : {
+                            hp : 4700,
+                            battery : 3,
+                            active : 3000
+                        }
+                    }
+                };
+                assert.deepEqual(data,expect,'ダメージフェイズのデータが正しい');
+                sendDamagePhaseCommand();
+            }
+
+            function sendDamagePhaseCommand(){
+                client.emit('command',{
+                    method : 'ok'
+                });
+                client.once('resp',waitPhase2);
+            }
+
+            function waitPhase2(data){
+                var expect = {
+                    phase : 'wait',
+                    atackUserId : 'test002@gmail.com',
+                    turn : 7,
+                    statusArray : {
+                        'nonePlayerCharacter' : {
+                            hp : 3200,
+                            battery : 5,
+                            active : 3500
+                        },
+                        'test002@gmail.com' : {
+                            hp : 4700,
+                            battery : 4,
+                            active : 5100
+                        }
+                    }
+                };
+                assert.deepEqual(data,expect,'2回目ウェイトフェイズのデータが正しい');
+                done();
+            }
+
+        });
+    });
+});
