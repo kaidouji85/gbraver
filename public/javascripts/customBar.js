353f36591b60f155274aef7a13581ccd3571d144
kaidouji85 <kaidouji85@gmail.com>
2014-11-02 03:02:25 +0900

    アクティブゲージを独自実装バーにした。

--- public/javascripts/customBar.js
+++ public/javascripts/customBar.js
@@ -1,80 +1,59 @@
-function customBar(spec, my) {
+function customBar(spec,my){
     var that = new Group();
     var barImage = spec.barImage;
     var direction = spec.direction;
     var maxValue = spec.maxValue;
     var backImage = spec.backImage;
-    var MainBar = mainBar({
-        image : barImage,
-        direction : direction,
-        maxValue : maxValue
-    });
-    var BarBack = barBack({
-        image : backImage,
-        direction : direction,
-        maxValue : maxValue
-    });
-    
-    that.addChild(BarBack);
-    that.addChild(MainBar);
-    
-    that.plus = MainBar.plus;
-    that.setValue = MainBar.setValue;
-    that.getValue = MainBar.getValue;
-    
-    return that;
-};
-
-function mainBar(spec, my) {
-    var that = new Bar(0, 0);
-    var image = spec.image;
-    var direction = spec.direction;
-    var maxValue = spec.maxValue;
+    var mainBar = new Sprite(maxValue,16);
+    var backBar = new Sprite(maxValue+2,18);
+    var value = 0;
     var turn = 0;
     var speed = 0;
 
-    that.value = 0;
-    that.image = image;
-    that.direction = direction;
-    that.maxValue = maxValue;
+    init();
+    function init(){
+        backBar.image = backImage;
+        backBar.x = (direction==='right' ? -1 : -maxValue-1);
+        backBar.y = -1;
+        that.addChild(backBar);
 
-    that.plus = function(p_turn, p_speed) {
+        mainBar.image = barImage;
+        mainBar.x = (direction==='right' ? 0 : 0);
+        that.addChild(mainBar);
+    }
+
+    that.plus = function(p_turn,p_speed){
         turn = p_turn;
         speed = p_speed;
-    };
+    }
+
+    that.setValue = function(p_value){
+        value = p_value;
+        fixBarWidth();
+    }
+
+    that.getValue = function(){
+        return value;
+    }
 
     that.addEventListener('enterframe', function(e) {
         if (turn > 0) {
-            that.setValue(that.value + speed);
+            value += speed;
             turn--;
         }
+        fixBarWidth();
     });
-    
-    that.setValue = function(value){
-        if(maxValue<value) {
-           value=maxValue; 
-        } else if(value<0){
-            value = 0;
-        }
-        that.value = value;        
-    };
 
-    that.getValue = function(){
-        return that.value;
+    function fixBarWidth(){
+        var width = value;
+        if(width > maxValue){
+            width = maxValue;
+        } else if(width < 0) {
+            width = 0;
+        }
+        mainBar.width = width;
+        mainBar.x = (direction==='right' ? 0 : -width);
     }
 
-    return that;
-}
-
-function barBack(spec,my){
-    var that = new Sprite(spec.maxValue+2,18);
-    var maxValue = spec.maxValue;
-    var direction = spec.direction;
-    var image = spec.image;
-    
-    that.image = image;
-    that.x = (direction==='right' ? -1 : -maxValue-1);
-    that.y = -1;
-    
     return that;
 }
