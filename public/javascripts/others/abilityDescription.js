ddc98563fc7446d3d82ba7d058d5c9084bcf9dff
kaidouji85 <kaidouji85@gmail.com>
2015-03-31 22:26:46 +0900

    boostBatteryの説明文を修正した

--- public/javascripts/others/abilityDescription.js
+++ public/javascripts/others/abilityDescription.js
@@ -3,7 +3,7 @@ function getArmdozerAbilityDescription(ability){
     if(ability.type==='boostBattery'){
         ret = 'バッテリーが'+ability.battery+'回復';
     } else if (ability.type==='boostActive') {
-        ret = 'アクティブゲージが' + ability*100 +'%増加する';
+        ret = 'アクティブゲージが' + ability.active*100 +'%増加する';
     }
     return ret;
 }
