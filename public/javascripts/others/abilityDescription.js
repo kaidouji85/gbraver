708e4214a1b915f1493979dbd8e428f337994038
kaidouji85 <kaidouji85@gmail.com>
2015-04-18 14:06:30 +0900

    アビリティ説明文を修正

--- public/javascripts/others/abilityDescription.js
+++ public/javascripts/others/abilityDescription.js
@@ -5,10 +5,10 @@ function getArmdozerAbilityDescription(ability){
             ret = 'バッテリーが'+ability.battery+'回復';
             break;
         case 'boostActive':
-            ret = 'アクティブゲージが' + ability.active*100 +'%増加する';
+            ret = 'アクティブゲージが' + ability.active*100 +'%増加';
             break;
         case 'boostPower':
-            ret = '攻撃が'+ability.power+'増加する';
+            ret = '攻撃が'+ability.power+'増加';
             break;
     }
     return ret;
