7d08bbdccbf2f519f20c055091da791a2b9f4c90
kaidouji85 <kaidouji85@gmail.com>
2015-06-24 21:53:49 +0900

    敵ターンにターンタイマーが無効になるテストを追加した

---
+++ publicForTest/javascripts/battleSceneTest/turnTimer/invalidTimeOverForEnemyTurnTest.js
@@ -0,0 +1,122 @@
+enchant();
+window.onload = doTest;
+
+function doTest(){
+    var assert = chai.assert;
+    var testDataInst = testData();
+    var statusArray = {
+        'test002@gmail.com' : testDataInst.getPlayerData('test002@gmail.com').status,
+        'test001@gmail.com' : testDataInst.getPlayerData('test001@gmail.com').status
+    };
+    var Game;
+    initGame();
+
+    function initGame(){
+        Game = game({
+            userId : 'test002@gmail.com',
+            armdozerPict : 'Landozer.PNG'
+        });
+        Game.start();
+        Game.onload = function(){
+            Game.changeBattleScene({
+                timeOver : 5,
+                statusArray : statusArray
+            });
+            waitPhase();
+        };
+    }
+
+    function waitPhase(){
+        var waitPhaseData = {
+            phase : 'wait',
+            atackUserId : 'test001@gmail.com',
+            turn : 20,
+            statusArray : {
+                'test002@gmail.com' : {
+                    hp : 4700,
+                    battery : 5,
+                    active : 3000,
+                    skillPoint : 1,
+                    overHeatFlag : false
+                },
+                'test001@gmail.com' : {
+                    hp : 3200,
+                    battery : 5,
+                    active : 5000,
+                    skillPoint : 1,
+                    overHeatFlag : false
+                }
+            }
+        };
+        Game.emitServerResp('resp',waitPhaseData);
+        Game.onSendMessage(function(message,data){
+            //message、dataはenemyChargeTestで確認済み
+            Game.currentScene.tl.delay(30).then(atackCommandPhase);
+        });
+    }
+
+    function atackCommandPhase(){
+        var data = {
+            phase : 'atackCommand',
+            statusArray : {
+                'test001@gmail.com' : {
+                    hp : 3200,
+                    battery : 5,
+                    active : 5000,
+                    skillPoint : 1,
+                    overHeatFlag : false
+                },
+                'test002@gmail.com' : {
+                    hp : 4700,
+                    battery : 5,
+                    active : 3000,
+                    skillPoint : 1,
+                    overHeatFlag : false
+                }
+            }
+        };
+        Game.emitServerResp('resp',data);
+        Game.onSendMessage(defenthCommandPhase);
+    }
+
+    function defenthCommandPhase() {
+        var data = {
+            phase : 'defenthCommand',
+            statusArray : {
+                'test001@gmail.com' : {
+                    hp : 3200,
+                    battery : 5,
+                    active : 5000,
+                    skillPoint : 1,
+                    overHeatFlag : false
+                },
+                'test002@gmail.com' : {
+                    hp : 4700,
+                    battery : 5,
+                    active : 3000,
+                    skillPoint : 1,
+                    overHeatFlag : false
+                }
+            }
+        };
+        Game.emitServerResp('resp',data);
+        selectCommnad();
+    }
+
+    function selectCommnad(){
+        Game.onSendMessage(sendCommandForDefenseCommand);
+        touch(Game.currentScene.okIcon);
+    }
+
+    function sendCommandForDefenseCommand(){
+        assert.equal(Game.currentScene.playerTurnTimer.getVisible(),false,'ターンタイマーが表示されていない');
+        Game.onSendMessage(function(){
+            new Exception('メッセージ送信が2回呼ばれたのでエラー発生');
+        });
+        //3秒待つ
+        Game.currentScene.tl.delay(120).then(function(){
+            finishTest();
+        });
+    }
+
+}
