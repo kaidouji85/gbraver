2b76bf85657a3f983d6a9c11091fcf14283178d3
kaidouji85 <kaidouji85@gmail.com>
2016-04-02 00:43:10 +0900

    定数から自動的にリソースを読み込むようにした

--- src/game/gameBase.js
+++ src/game/gameBase.js
@@ -1,57 +1,80 @@
 var bgmManager = require('../bgm/bgmManager');
+var __ = require('underscore');
 
-module.exports = function(spec,my){
+module.exports = function(spec){
     var core = new Core(320, 480);
-    var contentBaseUrl = spec&&spec.contentBaseUrl || location.origin;
-    core.PICT_PREFIX = contentBaseUrl + '/images/';
-    core.PICT_ACTIVE_BAR = core.PICT_PREFIX+'activeBar.png';
-    core.PICT_ACTIVE_BAR_BACK = core.PICT_PREFIX+'activeBack.png';
-    core.PICT_BATTERY_GAUGE = core.PICT_PREFIX+'batteryGauge.png';
-    core.PICT_BATTERY_BACK = core.PICT_PREFIX+'batteryBack.png';
-    core.PICT_BATTERY_NUMBER = core.PICT_PREFIX+'batteryNumber.png';
-    core.PICT_HIT_EFFECT = core.PICT_PREFIX+'hitEffect.png';
-    core.PICT_DAMAGE = core.PICT_PREFIX+'damage.png';
-    core.PICT_HIT_TEXT = core.PICT_PREFIX+'hitText.png';
-    core.PICT_BG_GROUND = core.PICT_PREFIX+'bgGround.png';
-    core.PICT_BG_GROUND2 = core.PICT_PREFIX+'bgGround2.png';
-    core.PICT_WINDOW = core.PICT_PREFIX+'window.png';
-    core.PICT_ACTIVE_WINDOW = core.PICT_PREFIX+'activeWindow.png';
-    core.PICT_BLACK_WINDOW = core.PICT_PREFIX+'blackWindow.png';
-    core.PICT_COMMAND_WINDOW = core.PICT_PREFIX+'commandWindow.png';
-    core.PICT_WIN = core.PICT_PREFIX+'win.png';
-    core.PICT_LOSE = core.PICT_PREFIX+'lose.png';
-    core.PICT_BUTTON = core.PICT_PREFIX+'button.png';
-    core.PICT_EXPLOSION = core.PICT_PREFIX+'explosion.png';
-    core.PICT_OVERHEAT_BUTTON = core.PICT_PREFIX+'overHeatButton.png';
-    core.PICT_WAKEUP_BACK = core.PICT_PREFIX+'wakeUpBack.png';
-    core.PICT_EXECUTE_ABILITY_TELOP = core.PICT_PREFIX+'executeAbility.png';
-    core.PICT_EXECUTE_SKILL_TELOP = core.PICT_PREFIX+'executePilotSkill.png';
-    core.PICT_MINI_NUMBER = core.PICT_PREFIX+'miniNumber.png';
-    core.PICT_SPECIAL_BAR = core.PICT_PREFIX+'specialBar.png';
-    core.PICT_SPECIAL_BAR_BACK = core.PICT_PREFIX+'specialBarBack.png';
-    core.PICT_BASIC_MERTER_BASE = core.PICT_PREFIX+'basicMerterBase.png';
-    core.PICT_HP_MERTER_UP = core.PICT_PREFIX+'hpMerterUp.png';
-    core.PICT_HP_MERTER_DOWN = core.PICT_PREFIX+'hpMerterDown.png';
-    core.PICT_BASIC_MERTER_NUMBER_BACK = core.PICT_PREFIX+'basicMerterNumberBack.png';
-    core.PICT_ACTIVE_MERTER_UP = core.PICT_PREFIX+'activeMerterUp.png';
-    core.PICT_ACTIVE_MERTER_DOWN = core.PICT_PREFIX+'activeMerterDown.png';
-    core.PICT_BATTERY_MERTER_UP = core.PICT_PREFIX+'batteryMerterUp.png';
-    core.PICT_BATTERY_MERTER_DOWN = core.PICT_PREFIX+'batteryMerterDown.png';
-    core.PICT_SPECIAL_MERTER_UP = core.PICT_PREFIX+'specialMerterUp.png';
-    core.PICT_SPECIAL_MERTER_DOWN = core.PICT_PREFIX+'specialMerterDown.png';
-    core.PICT_TOURNAMENT_BLOCK_1 = core.PICT_PREFIX+'tournamentBlock1.png';
-    core.PICT_TOURNAMENT_BLOCK_2 = core.PICT_PREFIX+'tournamentBlock2.png';
-    core.PICT_TOURNAMENT_BLOCK_3 = core.PICT_PREFIX+'tournamentBlock3.png';
+    var contentBaseUrl = spec && spec.contentBaseUrl || location.origin;
+
+    core.PICT_PREFIX = '/images/';
+    var pictResource = {
+        PICT_GRAN_BRAVER: core.PICT_PREFIX+'GranBraver.PNG',
+        PICT_WAKE_UP_GRANBRAVER: core.PICT_PREFIX+'wakeUpGranBraver.PNG',
+        PICT_LANDOZER: core.PICT_PREFIX+'Landozer.PNG',
+        PICT_WAKE_UP_LANDOZER: core.PICT_PREFIX+'wakeUpLandozer.PNG',
+        PICT_GUARDIAS: core.PICT_PREFIX+'Guardias.png',
+        PICT_GUARDIAS_BREAK: core.PICT_PREFIX+'GuardiasBreak.png',
+        PICT_WAKE_UP_GUARDIAS: core.PICT_PREFIX+'wakeUpGuardias.png',
+
+        PICT_ZERO_BRAVER: core.PICT_PREFIX+'ZeroBraver.PNG',
+        PICT_WAKE_UP_ZERO_BRAVER: core.PICT_PREFIX+'wakeUpZeroBraver.PNG',
+        PICT_KYOKO: core.PICT_PREFIX+'kyoko.png',
+        PICT_AKANE: core.PICT_PREFIX+'akane.png',
+        PICT_IORI: core.PICT_PREFIX+'iori.png',
+        PICT_AKIRA: core.PICT_PREFIX+'akira.png',
+        PICT_UKETUKE: core.PICT_PREFIX+'uketuke.png',
+        PICT_UNTENSYU: core.PICT_PREFIX+'untensyu.png',
+
+        PICT_ACTIVE_BAR: core.PICT_PREFIX+'activeBar.png',
+        PICT_ACTIVE_BAR_BACK: core.PICT_PREFIX+'activeBack.png',
+        PICT_BATTERY_GAUGE: core.PICT_PREFIX+'batteryGauge.png',
+        PICT_BATTERY_BACK: core.PICT_PREFIX+'batteryBack.png',
+        PICT_BATTERY_NUMBER: core.PICT_PREFIX+'batteryNumber.png',
+        PICT_HIT_EFFECT: core.PICT_PREFIX+'hitEffect.png',
+        PICT_DAMAGE: core.PICT_PREFIX+'damage.png',
+        PICT_HIT_TEXT: core.PICT_PREFIX+'hitText.png',
+        PICT_BG_GROUND: core.PICT_PREFIX+'bgGround.png',
+        PICT_BG_GROUND2: core.PICT_PREFIX+'bgGround2.png',
+        PICT_WINDOW: core.PICT_PREFIX+'window.png',
+        PICT_ACTIVE_WINDOW: core.PICT_PREFIX+'activeWindow.png',
+        PICT_BLACK_WINDOW: core.PICT_PREFIX+'blackWindow.png',
+        PICT_COMMAND_WINDOW: core.PICT_PREFIX+'commandWindow.png',
+        PICT_WIN: core.PICT_PREFIX+'win.png',
+        PICT_LOSE: core.PICT_PREFIX+'lose.png',
+        PICT_BUTTON: core.PICT_PREFIX+'button.png',
+        PICT_EXPLOSION: core.PICT_PREFIX+'explosion.png',
+        PICT_OVERHEAT_BUTTON: core.PICT_PREFIX+'overHeatButton.png',
+        PICT_WAKEUP_BACK: core.PICT_PREFIX+'wakeUpBack.png',
+        PICT_EXECUTE_ABILITY_TELOP: core.PICT_PREFIX+'executeAbility.png',
+        PICT_EXECUTE_SKILL_TELOP: core.PICT_PREFIX+'executePilotSkill.png',
+        PICT_MINI_NUMBER: core.PICT_PREFIX+'miniNumber.png',
+        PICT_SPECIAL_BAR: core.PICT_PREFIX+'specialBar.png',
+        PICT_SPECIAL_BAR_BACK: core.PICT_PREFIX+'specialBarBack.png',
+        PICT_BASIC_MERTER_BASE: core.PICT_PREFIX+'basicMerterBase.png',
+        PICT_HP_MERTER_UP: core.PICT_PREFIX+'hpMerterUp.png',
+        PICT_HP_MERTER_DOWN: core.PICT_PREFIX+'hpMerterDown.png',
+        PICT_BASIC_MERTER_NUMBER_BACK: core.PICT_PREFIX+'basicMerterNumberBack.png',
+        PICT_ACTIVE_MERTER_UP: core.PICT_PREFIX+'activeMerterUp.png',
+        PICT_ACTIVE_MERTER_DOWN: core.PICT_PREFIX+'activeMerterDown.png',
+        PICT_BATTERY_MERTER_UP: core.PICT_PREFIX+'batteryMerterUp.png',
+        PICT_BATTERY_MERTER_DOWN: core.PICT_PREFIX+'batteryMerterDown.png',
+        PICT_SPECIAL_MERTER_UP: core.PICT_PREFIX+'specialMerterUp.png',
+        PICT_SPECIAL_MERTER_DOWN: core.PICT_PREFIX+'specialMerterDown.png',
+        PICT_TOURNAMENT_BLOCK_1: core.PICT_PREFIX+'tournamentBlock1.png',
+        PICT_TOURNAMENT_BLOCK_2: core.PICT_PREFIX+'tournamentBlock2.png',
+        PICT_TOURNAMENT_BLOCK_3: core.PICT_PREFIX+'tournamentBlock3.png'
+    };
 
     core.SOUND_PREFIX = contentBaseUrl+'/sound/';
-    core.SOUND_BATTLE = core.SOUND_PREFIX + 'game_maoudamashii_1_battle34.mp3';
-    core.SOUND_ATTACK_HIT = core.SOUND_PREFIX + 'mecha27.wav';
-    core.SOUND_ATTACK_MISS = core.SOUND_PREFIX + 'swing32.wav';
-    core.SOUND_MY_TURN = core.SOUND_PREFIX + 'cursor02.wav';
-    core.SOUND_EXPLOSION = core.SOUND_PREFIX + 'tm2r_bom27_a.wav';
-    core.SOUND_PUSH_BUTTON = core.SOUND_PREFIX + 'tm2_switch001.wav';
-    core.SOUND_OPEN_BATTERY = core.SOUND_PREFIX + 'power21.wav';
-    core.SOUND_WAKE_UP = core.SOUND_PREFIX + 'mecha20.wav';
+    var soundResource = {
+        SOUND_BATTLE : core.SOUND_PREFIX + 'game_maoudamashii_1_battle34.mp3',
+        SOUND_ATTACK_HIT : core.SOUND_PREFIX + 'mecha27.wav',
+        SOUND_ATTACK_MISS : core.SOUND_PREFIX + 'swing32.wav',
+        SOUND_MY_TURN : core.SOUND_PREFIX + 'cursor02.wav',
+        SOUND_EXPLOSION : core.SOUND_PREFIX + 'tm2r_bom27_a.wav',
+        SOUND_PUSH_BUTTON : core.SOUND_PREFIX + 'tm2_switch001.wav',
+        SOUND_OPEN_BATTERY : core.SOUND_PREFIX + 'power21.wav',
+        SOUND_WAKE_UP : core.SOUND_PREFIX + 'mecha20.wav'
+    };
 
     core.WAIT_TIME_ACTIVE_RESET = 30;
     core.ICON_WIDTH = 124;
@@ -82,72 +105,13 @@ module.exports = function(spec,my){
 
     core.fps = 40;
     core.bgm = bgmManager();
-    preLoad();
 
-    //TODO 定数からの自動読み込みにする
-    function preLoad() {
-        core.preload(core.PICT_PREFIX+'GranBraver.PNG');
-        core.preload(core.PICT_PREFIX+'wakeUpGranBraver.PNG');
-        core.preload(core.PICT_PREFIX+'Landozer.PNG');
-        core.preload(core.PICT_PREFIX+'Guardias.png');
-        core.preload(core.PICT_PREFIX+'GuardiasBreak.png');
-        core.preload(core.PICT_PREFIX+'wakeUpGuardias.png');
-        core.preload(core.PICT_PREFIX+'wakeUpLandozer.PNG');
-        core.preload(core.PICT_PREFIX+'ZeroBraver.PNG');
-        core.preload(core.PICT_PREFIX+'wakeUpZeroBraver.PNG');
-        core.preload(core.PICT_PREFIX+'kyoko.png');
-        core.preload(core.PICT_PREFIX+'akane.png');
-        core.preload(core.PICT_PREFIX+'iori.png');
-        core.preload(core.PICT_PREFIX+'akira.png');
-        core.preload(core.PICT_PREFIX+'uketuke.png');
-        core.preload(core.PICT_PREFIX+'untensyu.png');
-        core.preload(core.PICT_ACTIVE_BAR);
-        core.preload(core.PICT_ACTIVE_BAR_BACK);
-        core.preload(core.PICT_BATTERY_GAUGE);
-        core.preload(core.PICT_BATTERY_BACK);
-        core.preload(core.PICT_BATTERY_NUMBER);
-        core.preload(core.PICT_HIT_EFFECT);
-        core.preload(core.PICT_DAMAGE);
-        core.preload(core.PICT_HIT_TEXT);
-        core.preload(core.PICT_BG_GROUND);
-        core.preload(core.PICT_BG_GROUND2);
-        core.preload(core.PICT_WINDOW);
-        core.preload(core.PICT_ACTIVE_WINDOW);
-        core.preload(core.PICT_BLACK_WINDOW);
-        core.preload(core.PICT_COMMAND_WINDOW);
-        core.preload(core.PICT_WIN);
-        core.preload(core.PICT_LOSE);
-        core.preload(core.PICT_BUTTON);
-        core.preload(core.PICT_EXPLOSION);
-        core.preload(core.PICT_OVERHEAT_BUTTON);
-        core.preload(core.PICT_WAKEUP_BACK);
-        core.preload(core.PICT_EXECUTE_ABILITY_TELOP);
-        core.preload(core.PICT_EXECUTE_SKILL_TELOP);
-        core.preload(core.PICT_MINI_NUMBER);
-        core.preload(core.PICT_SPECIAL_BAR);
-        core.preload(core.PICT_SPECIAL_BAR_BACK);
-        core.preload(core.PICT_BASIC_MERTER_BASE);
-        core.preload(core.PICT_HP_MERTER_UP);
-        core.preload(core.PICT_HP_MERTER_DOWN);
-        core.preload(core.PICT_BASIC_MERTER_NUMBER_BACK);
-        core.preload(core.PICT_ACTIVE_MERTER_UP);
-        core.preload(core.PICT_ACTIVE_MERTER_DOWN);
-        core.preload(core.PICT_BATTERY_MERTER_UP);
-        core.preload(core.PICT_BATTERY_MERTER_DOWN);
-        core.preload(core.PICT_SPECIAL_MERTER_UP);
-        core.preload(core.PICT_SPECIAL_MERTER_DOWN);
-        core.preload(core.PICT_TOURNAMENT_BLOCK_1);
-        core.preload(core.PICT_TOURNAMENT_BLOCK_2);
-        core.preload(core.PICT_TOURNAMENT_BLOCK_3);
+    __.each(pictResource, function(item) {
+        core.preload(item);
+    });
+    __.each(soundResource, function(item) {
+        core.preload(item);
+    });
 
-        core.preload(core.SOUND_BATTLE);
-        core.preload(core.SOUND_ATTACK_HIT);
-        core.preload(core.SOUND_ATTACK_MISS);
-        core.preload(core.SOUND_MY_TURN);
-        core.preload(core.SOUND_EXPLOSION);
-        core.preload(core.SOUND_PUSH_BUTTON);
-        core.preload(core.SOUND_OPEN_BATTERY);
-        core.preload(core.SOUND_WAKE_UP);
-    }
-    return core;
+    return __.extend(core, pictResource, soundResource);
 }
