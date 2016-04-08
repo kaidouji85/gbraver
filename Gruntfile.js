838c4c764fb494d87910bc3ebfb531b9c8ac1381
kaidouji85 <kaidouji85@gmail.com>
2016-04-08 23:37:18 +0900

    s3ファイルアップロードジョブをgrunt-aws-s3作り直した

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,18 +5,18 @@ module.exports = function(grunt) {
     var config = grunt.file.readJSON('./GruntConfig.json');
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        s3: require('./grunt/s3')(config.s3),
+        aws_s3: require('./grunt/aws_s3')(config.s3),
         exec: require('./grunt/exec')(config.mongo),
         webpack: require('./grunt/webpack'),
         clean: require('./grunt/clean'),
     });
 
-    grunt.loadNpmTasks('grunt-s3');
     grunt.loadNpmTasks('grunt-exec');
     grunt.loadNpmTasks('grunt-webpack');
     grunt.loadNpmTasks('grunt-contrib-clean');
-
-    grunt.registerTask('deploy',['s3','exec:mongo','exec:pushHeroku']);
+    grunt.loadNpmTasks('grunt-aws-s3');
+    
+    grunt.registerTask('deploy',['aws_s3:cleanProduct','aws_s3:uploadProduct','exec:mongo','exec:pushHeroku']);
     grunt.registerTask('deployBeta',['exec:mongoBeta','exec:pushHerokuBeta']);
     grunt.registerTask('build', ['clean:product', 'webpack:product']);
     grunt.registerTask('buildTest', ['clean:test', 'webpack:test']);
